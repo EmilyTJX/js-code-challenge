bced23e0fab682ec7093bc28daa6199de57c3213
Emily <tockemily@yahoo.com.sg>
2021-03-06 23:39:09 +0800

    Update qn1 solutions a,b,c

--- src/three_ways_to_sum_to_n/solution_b.js
+++ src/three_ways_to_sum_to_n/solution_b.js
@@ -3,6 +3,9 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+//using arithmetic progression
+var sum_to_nB = function (n) {
+    return ((n * (n + 1)) / 2);
 };
+
+console.log(sum_to_nB(5));
--- src/three_ways_to_sum_to_n/solution_c.js
+++ src/three_ways_to_sum_to_n/solution_c.js
@@ -3,6 +3,15 @@
 // Input: `n` - any integer from `0` to `Number.MAX_SAFE_INTEGER`.
 // Output: `return` - summation to `n`, i.e. sum_to_n(5) === 1 + 2 + 3 + 4 + 5 === 15.
 
-var sum_to_n = function(n) {
-    // your code here
+//using recursion to sum n
+var sum_to_nC = function (n) {
+
+    if (n <= 1) {   //base case
+        return n;
+    } else {
+        return n + sum_to_nC(n - 1);
+
+    }
 };
+
+console.log(sum_to_nC(5));
